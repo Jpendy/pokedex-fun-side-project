d210ae6b1e4890eb11d374c957bce5180cd8d30d
Jpendy <Jpendy256@gmail.com>
2020-04-04 11:04:20 -0700

    added new file pokedexArray.js to import array of pokemon objects

---
+++ pokedexArray.js
@@ -0,0 +1,74 @@
+export const pokedexArray = [
+    {
+        name: 'Charmander',
+        image: 'https://pm1.narvii.com/6458/d74415aec2cc1cece777a76509707051d9ed5825_hq.jpg',
+        type: 'Type: Fire'
+    },
+
+    {
+        name: 'Charmeleon',
+        image: 'https://img.pokemondb.net/artwork/large/charmeleon.jpg',
+        type: 'Type: Fire'
+    },
+    
+    {
+        name: 'Charizard',
+        image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
+        type: 'Type: Fire / Flying / Dragon'
+    },
+    
+    {
+        name: 'Squirtle',
+        image: 'https://2.bp.blogspot.com/-2W-zSozUe1Q/VR4IcZ7z9rI/AAAAAAAAAM4/M_dPUTPez8E/s1600/Squirtle.jpg',
+        type: 'Type: Water'
+    },
+    
+    {
+        name: 'Wartortle',
+        image: 'https://img.pokemondb.net/artwork/large/wartortle.jpg',
+        type: 'Type: Water'
+    },
+    
+    {
+        name: 'Blastoise',
+        image: 'https://usercontent2.hubstatic.com/13815739.jpg',
+        type: 'type: Water'
+    },
+    
+    {
+        name: 'Bulbasaur',
+        image: 'https://lh3.googleusercontent.com/proxy/acBm0V6ma9oEayE-oahaIqh6GPEvjjomtvU2XMObc7JjGa_S_EZ0_CEsCUZiW6kvJzJttp0t1rvbvZXwHLJDzE7c3AK5QLhqfHq5iPrj19t6Tza38Q',
+        type: 'Type: Grass'
+    },
+    
+    {
+        name: 'Ivysaur',
+        image: 'https://www.pngitem.com/pimgs/m/480-4806550_ivysaur-png-png-download-imagen-de-ivysaur-png.png',
+        type: 'Type: Grass'
+    },
+    
+    {
+        name: 'Venusaur',
+        image: 'https://www.pngitem.com/pimgs/m/155-1556739_female-venusaur-png-download-pokemon-venusaur-transparent-png.png',
+        type: 'Type: Grass'
+    },
+    
+    {
+        name: 'Pikachu',
+        image: 'https://webstockreview.net/images/pikachu-clipart-small-4.png',
+        type: 'Type: Electric'
+    },
+    
+    {
+        name: 'Raichu',
+        image: 'https://static.pokemonpets.com/images/monsters-images-300-300/26-Raichu.png',
+        type: 'Type: Electric'
+    },
+
+    {
+        name: 'Pidgey',
+        image: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/7/78/Pidgey_AG_anime.png/revision/latest/scale-to-width-down/340?cb=20130708224313',
+        type: 'Type: Flying'
+    }
+
+];
