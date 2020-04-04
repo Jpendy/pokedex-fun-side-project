985ee9a4902948636be3bc40620fafd7ad2a0b35
Jpendy <Jpendy256@gmail.com>
2020-04-04 14:31:56 -0700

    just got the search function to accept lowercase names

--- app.js
+++ app.js
@@ -5,14 +5,33 @@ const leftButton = document.getElementById('leftButton');
 const rightButton = document.getElementById('rightButton');
 const pokemonNameSpan = document.getElementById('pokemon-name');
 const pokemonTypeSpan = document.getElementById('pokemon-type');
-const searchInput = document.getElementById('search-input');
+let searchInputSpan = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
 let pokemonIndex = 0;
-let SearchValue = searchInput.value;
+
+//let searchInput = titleCase(searchInputSpan.value);
+
+//searchInputSpan.value = titleCase(searchInputSpan.value);
 
 pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
 image.src = pokedexArray[pokemonIndex].image;
+
+
+
+searchButton.addEventListener('click', () => {
+    let searchResult = search(searchInputSpan.value, pokedexArray);
+
+    pokemonNameSpan.textContent = searchResult.name;
+    pokemonTypeSpan.textContent = searchResult.type;
+    image.src = searchResult.image;
+    
+    let searchIndex = pokedexArray.indexOf(searchResult);
+    
+    pokemonIndex = searchIndex;
+    return pokemonIndex;
+});
+
 
 rightButton.addEventListener('click', () => {
     if (pokemonIndex < pokedexArray.length - 1) {
@@ -25,6 +44,7 @@ rightButton.addEventListener('click', () => {
 
 
 leftButton.addEventListener('click', () => {
+    console.log(pokemonIndex);
     if (pokemonIndex > 0) {
         pokemonIndex--;
         pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
@@ -34,98 +54,18 @@ leftButton.addEventListener('click', () => {
 });
 
 
-searchButton.addEventListener('click', () => {
-    let searchResult = search(searchInput.value, pokedexArray);
 
-    pokedexArray[pokemonIndex] = searchResult;
-    pokemonNameSpan.textContent = searchResult.name;
-    pokemonTypeSpan.textContent = searchResult.type;
-    image.src = searchResult.image;
-
-});
 
 
 function search(nameKey, myArray){
     for (let i = 0; i < myArray.length; i++){
-        if (myArray[i].name === nameKey) {
+        if (myArray[i].name === nameKey || myArray[i].lowerName === nameKey) {
             return myArray[i];
+
         }   
     }   
 }
 
-
-
-// let pokedexArray = [Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise, Bulbasaur, Ivysaur, Venusaur, Pikachu, Raichu, Pidgey];
-
-
-// const Charmander = {
-//     name: 'Charmander',
-//     image: 'https://pm1.narvii.com/6458/d74415aec2cc1cece777a76509707051d9ed5825_hq.jpg',
-//     type: 'Type: Fire'
-// };
-
-// const Charmeleon = {
-//     name: 'Charmeleon',
-//     image: 'https://img.pokemondb.net/artwork/large/charmeleon.jpg',
-//     type: 'Type: Fire'
-// };
-
-// const Charizard = {
-//     name: 'Charizard',
-//     image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
-//     type: 'Type: Fire / Flying / Dragon'
-// };
-
-// const Squirtle = {
-//     name: 'Squirtle',
-//     image: 'https://2.bp.blogspot.com/-2W-zSozUe1Q/VR4IcZ7z9rI/AAAAAAAAAM4/M_dPUTPez8E/s1600/Squirtle.jpg',
-//     type: 'Type: Water'
-// };
-
-// const Wartortle = {
-//     name: 'Wartortle',
-//     image: 'https://img.pokemondb.net/artwork/large/wartortle.jpg',
-//     type: 'Type: Water'
-// };
-
-// const Blastoise = {
-//     name: 'Blastoise',
-//     image: 'https://usercontent2.hubstatic.com/13815739.jpg',
-//     type: 'type: Water'
-// };
-
-// const Bulbasaur = {
-//     name: 'Bulbasaur',
-//     image: 'https://lh3.googleusercontent.com/proxy/acBm0V6ma9oEayE-oahaIqh6GPEvjjomtvU2XMObc7JjGa_S_EZ0_CEsCUZiW6kvJzJttp0t1rvbvZXwHLJDzE7c3AK5QLhqfHq5iPrj19t6Tza38Q',
-//     type: 'Type: Grass'
-// };
-
-// const Ivysaur = {
-//     name: 'Ivysaur',
-//     image: 'https://www.pngitem.com/pimgs/m/480-4806550_ivysaur-png-png-download-imagen-de-ivysaur-png.png',
-//     type: 'Type: Grass'
-// };
-
-// const Venusaur = {
-//     name: 'Venusaur',
-//     image: 'https://www.pngitem.com/pimgs/m/155-1556739_female-venusaur-png-download-pokemon-venusaur-transparent-png.png',
-//     type: 'Type: Grass'
-// };
-
-// const Pikachu = {
-//     name: 'Pikachu',
-//     image: 'https://webstockreview.net/images/pikachu-clipart-small-4.png',
-//     type: 'Type: Electric'
-// };
-
-// const Raichu = {
-//     name: 'Raichu',
-//     image: 'https://static.pokemonpets.com/images/monsters-images-300-300/26-Raichu.png',
-//     type: 'Type: Electric'
-// };
-
-// const Pidgey = {
-//     name: 'Pidgey',
-//     image: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/7/78/Pidgey_AG_anime.png/revision/latest/scale-to-width-down/340?cb=20130708224313',
-//     type: 'Type: Flying'
-// };
+function titleCase(str) {
+    return str.replace(/\w\S/g, function(t) { return t.toUpperCase(); });
+}
--- pokedexArray.js
+++ pokedexArray.js
@@ -1,72 +1,84 @@
 export const pokedexArray = [
     {
         name: 'Charmander',
+        lowerName: 'charmander',
         image: 'https://pm1.narvii.com/6458/d74415aec2cc1cece777a76509707051d9ed5825_hq.jpg',
         type: 'Type: Fire'
     },
 
     {
         name: 'Charmeleon',
+        lowerName: 'charmeleon',
         image: 'https://img.pokemondb.net/artwork/large/charmeleon.jpg',
         type: 'Type: Fire'
     },
     
     {
         name: 'Charizard',
+        lowerName: 'charizard',
         image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
         type: 'Type: Fire / Flying / Dragon'
     },
     
     {
         name: 'Squirtle',
+        lowerName: 'squirtle',
         image: 'https://2.bp.blogspot.com/-2W-zSozUe1Q/VR4IcZ7z9rI/AAAAAAAAAM4/M_dPUTPez8E/s1600/Squirtle.jpg',
         type: 'Type: Water'
     },
     
     {
         name: 'Wartortle',
+        lowerName: 'wartortle',
         image: 'https://img.pokemondb.net/artwork/large/wartortle.jpg',
         type: 'Type: Water'
     },
     
     {
         name: 'Blastoise',
+        lowerName: 'blastoise',
         image: 'https://usercontent2.hubstatic.com/13815739.jpg',
         type: 'type: Water'
     },
     
     {
         name: 'Bulbasaur',
+        lowerName: 'bulbasaur',
         image: 'https://images2.wikia.nocookie.net/__cb20111221113429/bulbasaurfanon/images/4/43/Bulbasaur.png',
         type: 'Type: Grass'
     },
     
     {
         name: 'Ivysaur',
+        lowerName: 'ivysaur',
         image: 'https://www.pngitem.com/pimgs/m/480-4806550_ivysaur-png-png-download-imagen-de-ivysaur-png.png',
         type: 'Type: Grass'
     },
     
     {
         name: 'Venusaur',
+        lowerName: 'venusaur',
         image: 'https://www.pngitem.com/pimgs/m/155-1556739_female-venusaur-png-download-pokemon-venusaur-transparent-png.png',
         type: 'Type: Grass'
     },
     
     {
         name: 'Pikachu',
+        lowerName: 'pikachu',
         image: 'https://webstockreview.net/images/pikachu-clipart-small-4.png',
         type: 'Type: Electric'
     },
     
     {
         name: 'Raichu',
+        lowerName: 'raichu',
         image: 'https://static.pokemonpets.com/images/monsters-images-300-300/26-Raichu.png',
         type: 'Type: Electric'
     },
 
     {
         name: 'Pidgey',
+        lowerName: 'pidgey',
         image: 'https://vignette.wikia.nocookie.net/sonicpokemon/images/7/78/Pidgey_AG_anime.png/revision/latest/scale-to-width-down/340?cb=20130708224313',
         type: 'Type: Flying'
     }
