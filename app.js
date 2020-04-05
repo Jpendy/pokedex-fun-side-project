b2b4c9ec0e13c69629a9f297d646a62e302d5d47
Jpendy <Jpendy256@gmail.com>
2020-04-05 14:38:25 -0700

    refactored the update function and added some good comments describing each part of the javascript

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-import { pokedexArray } from './pokedexArray.js'
+import { pokedexArray } from './pokedexArray.js';
+
+//This makes it so the arrow keys don't scroll the window. Also disables spacebar..
+window.addEventListener('keydown', function(e) {
+    // space and arrow keys
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 const image = document.getElementById('image');
 const leftButton = document.getElementById('leftButton');
@@ -8,7 +16,58 @@ const pokemonTypeSpan = document.getElementById('pokemon-type');
 let searchInputSpan = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
-//This makes the search button react to the enter key being clicked:
+
+
+
+//This here is integral, need to initialize the index (i) of the array. Have to do this
+// if you are not using a for loop to initialize it:
+let pokemonIndex = 0;
+
+pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
+image.src = pokedexArray[pokemonIndex].image;
+
+
+//Here are the two buttons.
+rightButton.addEventListener('click', () => {
+    //This if statement below is imortant so pressing left doesn't move the array out of the 
+    // 0 - array.length range. Otherwise it will past the limits of the array:
+    if (pokemonIndex < pokedexArray.length - 1) {
+        pokemonIndex++;
+        upDate();       
+    }
+});
+
+leftButton.addEventListener('click', () => {
+    //similar if statement here but different for the other direction decrimating
+    if (pokemonIndex > 0) {
+        pokemonIndex--;        
+        upDate();        
+    }
+});
+
+
+//This is the button that triggers the search function
+searchButton.addEventListener('click', () => {
+
+//Here we put the input.value and the array in as parameters, and it returns the
+//array[i] index that we desire. that is returned and contained in the variable searchResult:
+    let searchResult = search(searchInputSpan.value, pokedexArray);
+
+//This part uses the index held by searchResult to update all the relevant
+//spans and images on the view. This part should be refactored into a function of its own.
+    pokemonNameSpan.textContent = searchResult.name;
+    pokemonTypeSpan.textContent = searchResult.type;
+    image.src = searchResult.image;
+    
+//This part is Integral. This updates the index of the array to match the updates of the
+// key value pairs we just did above. Without this the view will be updated but the index won't.  
+    let searchIndex = pokedexArray.indexOf(searchResult);
+    pokemonIndex = searchIndex;
+    return pokemonIndex;
+});
+
+
+//This makes the search button react to the enter key being pressed:
 document.getElementById('search-input').addEventListener('keyup', function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -16,60 +75,41 @@ document.getElementById('search-input').addEventListener('keyup', function(event
     }
 });
 
-//This here is integral, need to set the index of the array:
-let pokemonIndex = 0;
 
-pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
-image.src = pokedexArray[pokemonIndex].image;
-
-
-searchButton.addEventListener('click', () => {
-    let searchResult = search(searchInputSpan.value, pokedexArray);
-
-    pokemonNameSpan.textContent = searchResult.name;
-    pokemonTypeSpan.textContent = searchResult.type;
-    image.src = searchResult.image;
-    
-    let searchIndex = pokedexArray.indexOf(searchResult);
-    
-    pokemonIndex = searchIndex;
-    return pokemonIndex;
-});
-
-
-rightButton.addEventListener('click', () => {
-    if (pokemonIndex < pokedexArray.length - 1) {
-        pokemonIndex++;
-        pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
-        image.src = pokedexArray[pokemonIndex].image;
-        pokemonTypeSpan.textContent = pokedexArray[pokemonIndex].type;
-    }
-});
-
-
-leftButton.addEventListener('click', () => {
-    console.log(pokemonIndex);
-    if (pokemonIndex > 0) {
-        pokemonIndex--;
-        pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
-        pokemonTypeSpan.textContent = pokedexArray[pokemonIndex].type;
-        image.src = pokedexArray[pokemonIndex].image;
-    }
-});
-
-
-
-
-
+//This is the search function. nameKey represents an Input.value It takes in the search 
+//from the input.value and check to  see if it matches the value in the array key. 
+//If it does it returns the array index:
 function search(nameKey, myArray){
     for (let i = 0; i < myArray.length; i++){
         if (myArray[i].name === nameKey || myArray[i].lowerName === nameKey) {
             return myArray[i];
-
         }   
     }   
 }
 
-function titleCase(str) {
-    return str.replace(/\w\S/g, function(t) { return t.toUpperCase(); });
+
+//This function updates the view when the left and right buttons are clicked:
+function upDate(){
+    pokemonNameSpan.textContent = pokedexArray[pokemonIndex].name;
+    pokemonTypeSpan.textContent = pokedexArray[pokemonIndex].type;
+    image.src = pokedexArray[pokemonIndex].image;
 }
+
+
+
+//This is a bunch of shit I could'nt get to work. Trying to make the right and left buttons
+//be clicked when you press the right and left arrow keys. Will fix later.
+
+// document.getElementById('leftButton').addEventListener('keyup', function(event){
+//     event.preventDefault();
+//     if (event.keyCode === 37) {
+//         document.getElementById('leftButton').click();
+//     }
+// });
+
+// document.getElementById('rightButton').addEventListener('keyup', function(event){
+//     event.preventDefault();
+//     if (event.keyCode === 39) {
+//         document.getElementById('rightButton').click();
+//     }
+// });
