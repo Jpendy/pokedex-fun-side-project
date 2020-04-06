9b3d4d1f2302336cfa04db5b0e8ad51a3f1dfec7
Jpendy <Jpendy256@gmail.com>
2020-04-05 21:48:28 -0700

    added descriptions for all pokemon in the array

--- pokedexArray.js
+++ pokedexArray.js
@@ -11,163 +11,184 @@ export const pokedexArray = [
         name: 'Charmeleon',
         lowerName: 'charmeleon',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/5-Charmeleon.png',
-        type: 'Type: Fire'
+        type: 'Type: Fire',
+        description: 'Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color.'
     },
     
     {
         name: 'Charizard',
         lowerName: 'charizard',
         image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
-        type: 'Type: Fire / Flying / Dragon'
+        type: 'Type: Fire / Flying / Dragon',
+        description: 'Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.'
     },
     
     {
         name: 'Squirtle',
         lowerName: 'squirtle',
         image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
-        type: 'Type: Water'
+        type: 'Type: Water',
+        description: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
     },
     
     {
         name: 'Wartortle',
         lowerName: 'wartortle',
         image: 'https://pokestop.io/img/pokemon/wartortle-256x256.png',
-        type: 'Type: Water'
+        type: 'Type: Water',
+        description: "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler."
     },
     
     {
         name: 'Blastoise',
         lowerName: 'blastoise',
         image: 'https://vignette.wikia.nocookie.net/monster/images/2/26/Blastoise_RG.png/revision/latest/scale-to-width-down/340?cb=20170806105613',
-        type: 'type: Water'
+        type: 'type: Water',
+        description: 'Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.'
     },
     
     {
         name: 'Bulbasaur',
         lowerName: 'bulbasaur',
         image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-        type: 'Type: Grass'
+        type: 'Type: Grass',
+        description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
     },
     
     {
         name: 'Ivysaur',
         lowerName: 'ivysaur',
         image: 'https://vignette.wikia.nocookie.net/pokemontowerdefense/images/7/73/002Ivysaur.png/revision/latest/scale-to-width-down/340?cb=20130614071842',
-        type: 'Type: Grass'
+        type: 'Type: Grass',
+        description: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon."
     },
     
     {
         name: 'Venusaur',
         lowerName: 'venusaur',
         image: 'https://pokestop.io/img/pokemon/venusaur-256x256.png',
-        type: 'Type: Grass'
+        type: 'Type: Grass',
+        description: "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people."
     },
 
     {
         name: 'Caterpie',
         lowerName: 'caterpie',
         image: 'https://vignette.wikia.nocookie.net/pokemontowerdefensetwo/images/6/63/010_large.png/revision/latest/scale-to-width-down/340?cb=20140413075858',
-        type: 'Type: Bug'
+        type: 'Type: Bug',
+        description: 'Caterpie has a voracious appetite. It can devour leaves bigger than its body right before your eyes. From its antenna, this Pokémon releases a terrifically strong odor.'
     },
     
     {
         name: 'Metapod',
         lowerName: 'metapod',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/11-Metapod.png',
-        type: 'Type: Bug'
+        type: 'Type: Bug',
+        description: "The shell covering this Pokémon's body is as hard as an iron slab. Metapod does not move very much. It stays still because it is preparing its soft innards for evolution inside the hard shell."
     },
 
     {
         name: 'Butterfree',
         lowerName: 'butterfree',
         image: 'https://vignette.wikia.nocookie.net/pokemon-adventures-spinoff-series/images/c/cf/012_butterfree_female_by_tzblacktd_da0qcev-fullview.png/revision/latest?cb=20190922021700',
-        type: 'Type: Bug / Flying'
+        type: 'Type: Bug / Flying',
+        description: 'Butterfree has a superior ability to search for delicious honey from flowers. It can even search out, extract, and carry honey from flowers that are blooming over six miles from its nest.'
     },
 
     {
         name: 'Weedle',
         lowerName: 'weedle',
         image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/85a954df-43b3-4b66-9e8d-94fe01b0451f/da0u6vt-e8c58b51-261d-47ae-8963-4b1949c37c2c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg1YTk1NGRmLTQzYjMtNGI2Ni05ZThkLTk0ZmUwMWIwNDUxZlwvZGEwdTZ2dC1lOGM1OGI1MS0yNjFkLTQ3YWUtODk2My00YjE5NDljMzdjMmMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.UHu59ZDsjxjZj6Yle-bETwaAjVGKi434J-ph_NzXKUE',
-        type: 'Type: Bug / Poison'
+        type: 'Type: Bug / Poison',
+        description: 'Weedle has an extremely acute sense of smell. It is capable of distinguishing its favorite kinds of leaves from those it dislikes just by sniffing with its big red proboscis (nose).'
     },
 
     {
         name: 'Kakuna',
         lowerName: 'kakuna',
         image: 'https://cdn.bulbagarden.net/upload/thumb/f/f0/014Kakuna.png/250px-014Kakuna.png',
-        type: 'Type: Bug / Poison'
+        type: 'Type: Bug / Poison',
+        description: 'Kakuna remains virtually immobile as it clings to a tree. However, on the inside, it is extremely busy as it prepares for its coming evolution. This is evident from how hot the shell becomes to the touch.'
     },
 
     {
         name: 'Beedrill',
         lowerName: 'beedrill',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/2015-Shiny-Beedrill.png',
-        type: 'Type: Bug / Poison'
+        type: 'Type: Bug / Poison',
+        description: 'Beedrill is extremely territorial. No one should ever approach its nest—this is for their own safety. If angered, they will attack in a furious swarm.'
     },
 
     {
         name: 'Pidgey',
         lowerName: 'pidgey',
         image: 'https://static.pokemonpets.com/images/monsters-images-300-300/16-Pidgey.png',
-        type: 'Type: Normal / Flying'
+        type: 'Type: Normal / Flying',
+        description: 'Pidgey has an extremely sharp sense of direction. It is capable of unerringly returning home to its nest, however far it may be removed from its familiar surroundings.'
     },
 
     {
         name: 'Pidgeotto',
         lowerName: 'pidgeotto',
         image: 'https://i.pinimg.com/originals/49/57/2d/49572d5ea2becc1379b06337a32c1887.png',
-        type: 'Type: Normal / Flying'
+        type: 'Type: Normal / Flying',
+        description: 'Pidgeotto claims a large area as its own territory. This Pokémon flies around, patrolling its living space. If its territory is violated, it shows no mercy in thoroughly punishing the foe with its sharp claws.'
     },
 
     {
         name: 'Pidgeot',
         lowerName: 'pidgeot',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/18-Pidgeot.png',
-        type: 'Type: Normal / Flying'
+        type: 'Type: Normal / Flying',
+        description: 'This Pokémon has a dazzling plumage of beautifully glossy feathers. Many Trainers are captivated by the striking beauty of the feathers on its head, compelling them to choose Pidgeot as their Pokémon.'
     },
 
     {
         name: 'Rattata',
         lowerName: 'rattata',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/2019-Shiny-Rattata.png',
-        type: 'Type: Normal'
+        type: 'Type: Normal',
+        description: 'Rattata is cautious in the extreme. Even while it is asleep, it constantly listens by moving its ears around. It is not picky about where it lives—it will make its nest anywhere.'
     },
 
     {
         name: 'Raticate',
         lowerName: 'raticate',
         image: 'https://static.pokemonpets.com/images/monsters-images-800-800/20-Raticate.png',
-        type: 'Type: Normal'
+        type: 'Type: Normal',
+        description: "Raticate's sturdy fangs grow steadily. To keep them ground down, it gnaws on rocks and logs. It may even chew on the walls of houses."
     },
 
     {
         name: 'Spearow',
         lowerName: 'spearow',
         image: 'https://static.pokemonpets.com/images/monsters-images-300-300/21-Spearow.png',
-        type: 'Type: Normal'
+        type: 'Type: Normal',
+        description: 'Spearow has a very loud cry that can be heard over half a mile away. If its high, keening cry is heard echoing all around, it is a sign that they are warning each other of danger.'
     },
 
     {
         name: 'Fearow',
         lowerName: 'fearow',
         image: 'https://vignette.wikia.nocookie.net/project-pokemon/images/4/41/Fearow.png/revision/latest?cb=20170511161826',
-        type: 'Type: Normal / Flying'
+        type: 'Type: Normal / Flying',
+        description: 'Fearow is recognized by its long neck and elongated beak. They are conveniently shaped for catching prey in soil or water. It deftly moves its long and skinny beak to pluck prey.'
     },
 
     {
         name: 'Pikachu',
         lowerName: 'pikachu',
         image: 'https://webstockreview.net/images/pikachu-clipart-small-4.png',
-        type: 'Type: Electric'
+        type: 'Type: Electric',
+        description: "Whenever Pikachu comes across something new, it blasts it with a jolt of electricity. If you come across a blackened berry, it's evidence that this Pokémon mistook the intensity of its charge."
     },
     
     {
         name: 'Raichu',
         lowerName: 'raichu',
         image: 'https://static.pokemonpets.com/images/monsters-images-300-300/26-Raichu.png',
-        type: 'Type: Electric'
+        type: 'Type: Electric',
+        description: "If the electrical sacs become excessively charged, Raichu plants its tail in the ground and discharges. Scorched patches of ground will be found near this Pokémon's nest."
     },
-
-   
 
 ];
